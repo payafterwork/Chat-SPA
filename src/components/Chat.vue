ab495ec7e3a6609ebf10135e34c935c56576d4be
Harshit Batra <payafterwork.in@gmail.com>
2019-08-20 16:34:30 +0530

    Displaying realtime messages from db

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="chat container">
-    <h2 class="center teal-text">Ninja Chat</h2>
     <div class="card">
       <div class="card-content">
         <ul class="messages">
-          <li>
-            <span class="teal-text">name</span>
-            <span class="grey-text text-darken-3">message</span>
-            <span class="grey-text time">time</span>
+          <li v-for="message in messages" :key="message.id">
+            <span class="teal-text">{{message.name}}</span>
+            <span class="grey-text text-darken-3">{{message.content}}</span>
+            <span class="grey-text time">{{message.timestamp}}</span>
           </li>
         </ul>
       </div>
@@ -19,7 +18,9 @@
 </template>
 
 <script>
+import db from '@/firebase/init'
 import NewMessage from '@/components/NewMessage'
+import moment from 'moment'
 export default {
   name: 'Chat',
   props: ['name'],
@@ -28,8 +29,28 @@ export default {
   },
   data(){
     return{
+    	messages: []
     }
-  }
+  },
+  created(){
+  	let ref = db.collection('messages').orderBy('timestamp');
+  	ref.onSnapshot(snapshot=>{
+        snapshot.docChanges().forEach(change=>{
+        	if(change.type=='added'){
+              let doc = change.doc 
+              this.messages.push({
+              	id: doc.id,
+              	name: doc.data().name,
+              	content: doc.data().content,
+              	timestamp: moment(doc.data().timestamp).format('lll')
+              })
+         }	
+        })
+        	
+  	})
+
+  }	
+  
 }
 </script>
 
@@ -44,5 +65,9 @@ export default {
 .chat .time{
   display: block;
   font-size: 1.2em;
+}
+.messages{
+	max-height:300px;
+	overflow:auto;
 }
 </style>
